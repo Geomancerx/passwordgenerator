faa6a3bcf7fbca7ea9608f8df0aef6c14d19b41e
geomancerx <geomancerx@yahoo.com>
2022-12-31 17:33:30 -0800

    password generator with a 10 character creation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # passwordgenerator
-randomly generates you a password
+# randomly generates you a password
+# to increase length of password you will need to edit line 6
+
---
+++ passwordgenerator.py
@@ -0,0 +1,27 @@
+# password generator
+
+import string
+import secrets
+
+def create_pw(pw_length=10):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+
+    alphabet = letters + digits + special_chars
+    pwd = ''
+    pwd_strong = False
+
+    while not pwd_strong:
+        pwd = ''
+        for i in range(pw_length):
+            pwd += ''.join(secrets.choice(alphabet))
+
+        if (any(char in special_chars for char in pwd) and
+                sum (char in digits for char in pwd) >= 2):
+            pwd_strong = True
+
+    return pwd
+
+if __name__ == '__main__':
+    print(create_pw())
